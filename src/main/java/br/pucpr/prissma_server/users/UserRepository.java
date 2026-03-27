4825ef43ef3c0246d2329454a6ff43ec1d476d8f
Giordano D. Serafini <gdserafini@outlook.com>
2026-03-27 19:09:53 -0300

    update user repository

--- src/main/java/br/pucpr/prissma_server/users/UserRepository.java
+++ src/main/java/br/pucpr/prissma_server/users/UserRepository.java
@@ -2,5 +2,8 @@ package br.pucpr.prissma_server.users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
