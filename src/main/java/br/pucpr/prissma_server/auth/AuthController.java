aff5f73ecbf43bbb7cc5ada49b4855067272ebd5
Giordano D. Serafini <gdserafini@outlook.com>
2026-03-29 13:35:47 -0300

    add login route

---
+++ src/main/java/br/pucpr/prissma_server/auth/AuthController.java
@@ -0,0 +1,23 @@
+package br.pucpr.prissma_server.auth;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService service;
+
+    public AuthController(AuthService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(service.login(request.email(), request.password()));
+    }
+}
---
+++ src/main/java/br/pucpr/prissma_server/auth/LoginRequest.java
@@ -0,0 +1,4 @@
+package br.pucpr.prissma_server.auth;
+
+public record LoginRequest(String email, String password) {
+}
