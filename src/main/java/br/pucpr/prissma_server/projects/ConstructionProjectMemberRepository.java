42d1f63b36114c6791e59caac8df7a4421403f2b
Giordano D. Serafini <gdserafini@outlook.com>
2026-05-17 16:35:11 -0300

    feat(project): update project repository

--- src/main/java/br/pucpr/prissma_server/projects/ConstructionProjectMemberRepository.java
+++ src/main/java/br/pucpr/prissma_server/projects/ConstructionProjectMemberRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ConstructionProjectMemberRepository extends JpaRepository<ConstructionProjectMember, Long> {
     @Query("SELECT m.constructionProject FROM ConstructionProjectMember m WHERE m.user.id = :userId")
     List<ConstructionProject> findAllProjectsByUserId(@Param("userId") Long userId);
+
+    Optional<ConstructionProjectMember> findByConstructionProjectIdAndUserId(Long constructionProjectId, Long userId);
 }
 
